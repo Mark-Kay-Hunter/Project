cd0921d9ebd43970e2a029aa3ffca7756cd3b335
mark2dx <mark2dx@kakao.com>
2019-11-08 09:29:56 +0900

    Outline Upload. Please Check This.

---
+++ src/main/java/kr/co/project/ScheduleController.java
@@ -0,0 +1,21 @@
+package kr.co.project;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/* 폴더별 Controller입니다. 각 폴더에 맞게 입력해주세요. */
+/* 꼬이면 피곤합니다. */
+
+@Controller
+
+public class ScheduleController {
+
+}
